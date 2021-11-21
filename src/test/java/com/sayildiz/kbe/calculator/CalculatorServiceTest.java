00a7fe7c37ff5ab6b2cec7fa3b2bb8cba5ca9560
sayildiz <y.samet97@gmail.com>
2021-11-21 20:11:00 +0100

    tests created

--- src/test/java/com/sayildiz/kbe/calculator/CalculatorServiceTest.java
+++ src/test/java/com/sayildiz/kbe/calculator/CalculatorServiceTest.java
@@ -2,15 +2,19 @@ package com.sayildiz.kbe.calculator;
 
 import com.sayildiz.kbe.calculator.model.Price;
 import com.sayildiz.kbe.calculator.service.CalculatorService;
+import com.sayildiz.kbe.calculator.exception.TooManyDecimalsException;
 import com.sayildiz.kbe.calculator.service.VATCalculatorService;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorServiceTest {
     CalculatorService service = new VATCalculatorService();
+    String tooManyDecimalMessage = "Value should have maximal two decimals ";
 
     @Test
-    public void testTax1(){
+    public void testVATCalculation1(){
         double price = 15.49;
         double expectedVAT = 2.94;
         double gross = price + expectedVAT;
@@ -34,5 +38,43 @@ public class CalculatorServiceTest {
         double gross = price + expectedTax;
         Price expectedPrice = new Price(gross, expectedTax, price);
         assertEquals(expectedPrice, service.calculateVAT(price));
+    }
+
+    @Test
+    public void testCheckDecimalWith3Decimals(){
+        double price = 3.123;
+        Exception exception = assertThrows(TooManyDecimalsException.class, () -> service.checkPrecision(price));
+
+        String expectedMessage = tooManyDecimalMessage + price;
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testCheckDecimalWith5Decimals(){
+        double price = 3.12366;
+        Exception exception = assertThrows(TooManyDecimalsException.class, () -> service.checkPrecision(price));
+
+        String expectedMessage = tooManyDecimalMessage + price;
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testCheckDecimalWithNoDecimals(){
+        double price = 3.;
+        service.checkPrecision(price);
+    }
+
+    @Test
+    public void testCheckDecimalWithOneDecimals(){
+        double price = 325.1;
+        service.checkPrecision(price);
+    }
+
+    @Test
+    public void testCheckDecimalWithTwoDecimals(){
+        double price = 334.77;
+        service.checkPrecision(price);
     }
 }
